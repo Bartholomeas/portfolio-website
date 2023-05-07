75c9ea562fc23b5273ddffd24d57ca496d2133ff
TMN <t.martin@adsoftware.fr>
2023-05-07 13:37:47 +0200

    fix: mantine ssr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
-import { Metadata } from 'next';
+import { Metadata } from "next";
 
-import ThemeProvider from '@/components/templates/ThemeProvider';
-import AppShell from '@/components/templates/AppShell';
+import ThemeProvider from "@/components/templates/ThemeProvider";
+import AppShell from "@/components/templates/AppShell";
+import RootStyleRegistry from "./emotion";
 
 export const metadata: Metadata = {
-  title: 'bartholomeas',
-  description: 'Bartholomeas website',
+  title: "bartholomeas",
+  description: "Bartholomeas website",
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
@@ -13,7 +14,9 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     <html lang="pl">
       <body suppressHydrationWarning={true}>
         <ThemeProvider>
-          <AppShell>{children}</AppShell>
+          <AppShell>
+            <RootStyleRegistry>{children}</RootStyleRegistry>
+          </AppShell>
         </ThemeProvider>
       </body>
     </html>
