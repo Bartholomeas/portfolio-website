07e1946e02f9e9bf1b35263cbc2957266914fc49
Bartholomeas <weivan321@gmail.com>
2023-10-01 22:24:59 +0200

    chore: minor fixes

--- frontend/app/components/views/landing/landing/components/LandingButtons.tsx
+++ frontend/app/components/views/landing/landing/components/LandingButtons.tsx
@@ -1,14 +1,14 @@
 import { IconArrowRight, IconPhone } from '@tabler/icons-react';
 import React from 'react';
 
-import { Flex, Group } from '@/components/common/mantine';
+import { Group } from '@/components/common/mantine';
 import { Button, ButtonLink } from '@/components/common/mantine/Button';
 
 import { routes } from '@/misc/routes';
 
 export function LandingButtons() {
   return (
-    <Group gap={8} justify="space-between" my={16} w="100%">
+    <Group spacing={8} my={16} w="100%">
       <ButtonLink
         href={routes.blog}
         color="primary"
