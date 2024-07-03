a9243a5aad3b188412ca2bebcb09f3ac4830d9dc
Bartosz <barth.webdesign@gmail.com>
2024-07-04 00:10:43 +0200

    [Chore]: improve avatars

--- frontend/app/components/views/about/sliders/AboutSummarySlider.tsx
+++ frontend/app/components/views/about/sliders/AboutSummarySlider.tsx
@@ -1,92 +1,93 @@
-import { createStyles } from '@mantine/core';
+import {createStyles} from '@mantine/core';
 
-import { Box } from '@/components/common/mantine';
-import { Image } from '@/components/common/mantine/Image';
-import { MacWindow } from '@/components/common/special/macWindow/MacWindow';
+import {Box} from '@/components/common/mantine';
+import {Image} from '@/components/common/mantine/Image';
+import {MacWindow} from '@/components/common/special/macWindow/MacWindow';
 
 const useStyles = createStyles(() => ({
-  root: {
-    position: 'relative',
-    display: 'flex',
-    width: '100%',
-    minHeight: 500,
-    overflow: 'visible',
-  },
+    root: {
+        position: 'relative',
+        display: 'flex',
+        width: '100%',
+        minHeight: 500,
+        overflow: 'visible',
+    },
 
-  firstWindow: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    overflow: 'visible',
-  },
-  emojiBox: {
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    left: 0,
-    bottom: 0,
-  },
+    firstWindow: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        overflow: 'visible',
+    },
+    emojiBox: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        left: 0,
+        bottom: 0,
+    },
 
-  image: {
-    objectFit: 'contain',
-  },
+    image: {
+        objectFit: 'contain',
+    },
 
-  firstImage: {
-    transform: 'translate(30%,-40%) scale(0.4) rotate(20deg)',
-  },
-  secondImage: {
-    transform: 'translate(-100px,-60%) scale(0.6) rotate(-10deg)',
-  },
+    firstImage: {
+        transform: 'translate(30%,-40%) scale(0.4) rotate(20deg)',
+    },
+    secondImage: {
+        transform: 'translate(-100px,-60%) scale(0.6) rotate(-10deg)',
+    },
 }));
 
 export function AboutSummarySlider() {
-  const { classes, cx } = useStyles();
+    const {classes, cx} = useStyles();
 
-  return (
-    <Box className={classes.root}>
-      <Image
-        src="/abstract/serpent_2.webp"
-        alt="Abstrakcyjny kolorowy kształt przypominający węża"
-        fill
-        sizes="50vw"
-        sx={{
-          position: 'absolute',
-        }}
-      />
+    return (
+        <Box className={classes.root}>
+            <Image
+                src="/abstract/serpent_2.webp"
+                alt="Abstrakcyjny kolorowy kształt przypominający węża"
+                fill
+                sizes="50vw"
+                sx={{
+                    position: 'absolute',
+                }}
+            />
 
-      <MacWindow withGlassBg className={classes.firstWindow}>
-        <Box className={classes.emojiBox}>
-          <Image
-            src="/avatars/me_hallo.webp"
-            alt="Emoji wykonujące gest dzwonienia."
-            width="0"
-            height="0"
-            sizes="100vw"
-            className={classes.image}
-            sx={{
-              position: 'absolute',
-              width: '100%',
-              maxHeight: 500,
-              height: 'auto',
-              bottom: 0,
-            }}
-          />
-          <Image
-            src="/avatars/me_blink.webp"
-            alt="Emoji mrugające lewym okiem."
-            fill
-            sizes="50vw"
-            className={cx(classes.image, classes.firstImage)}
-          />
-          <Image
-            src="/avatars/me_peace.webp"
-            alt="Emoji pokazujące znak pokoju."
-            fill
-            sizes="50vw"
-            className={cx(classes.image, classes.secondImage)}
-          />
+            <MacWindow withGlassBg className={classes.firstWindow}>
+                <Box className={classes.emojiBox}>
+                    <Image
+                        src="/avatars/me_hallo.png"
+                        alt="Emoji wykonujące gest dzwonienia."
+                        width={500}
+                        height={500}
+                        sizes="100vw"
+                        className={classes.image}
+                        sx={{
+                            position: 'absolute',
+                            width: '100%',
+                            maxHeight: 500,
+                            height: 'auto',
+                            transform: 'scaleX(-1)',
+                            bottom: 0,
+                        }}
+                    />
+                    <Image
+                        src="/avatars/me_blink.png"
+                        alt="Emoji mrugające lewym okiem."
+                        fill
+                        sizes="50vw"
+                        className={cx(classes.image, classes.firstImage)}
+                    />
+                    <Image
+                        src="/avatars/me_peace.png"
+                        alt="Emoji pokazujące znak pokoju."
+                        fill
+                        sizes="50vw"
+                        className={cx(classes.image, classes.secondImage)}
+                    />
+                </Box>
+            </MacWindow>
         </Box>
-      </MacWindow>
-    </Box>
-  );
+    );
 }
--- frontend/app/components/views/landing/projectRoad/ProjectRoadSection.tsx
+++ frontend/app/components/views/landing/projectRoad/ProjectRoadSection.tsx
@@ -1,144 +1,145 @@
 'use client';
 
 import React from 'react';
-import { createStyles } from '@mantine/core';
+import {createStyles} from '@mantine/core';
 
 import {
-  Icon,
-  IconBrain,
-  IconEaseInOutControlPoints,
-  IconHeartHandshake,
-  IconMoodCheck,
-  IconProgress,
+    Icon,
+    IconBrain,
+    IconEaseInOutControlPoints,
+    IconHeartHandshake,
+    IconMoodCheck,
+    IconProgress,
 } from '@tabler/icons-react';
 
 
-import { Box, Flex, Group, Stack } from '@/components/common/mantine';
-import { Image } from '@/components/common/mantine/Image';
-import { Text } from '@/components/common/mantine/Text';
-import { Title } from '@/components/common/mantine/Title';
-import { SectionHeading } from '@/components/common/ornaments/SectionHeading';
+import {Box, Flex, Group, Stack} from '@/components/common/mantine';
+import {Image} from '@/components/common/mantine/Image';
+import {Text} from '@/components/common/mantine/Text';
+import {Title} from '@/components/common/mantine/Title';
+import {SectionHeading} from '@/components/common/ornaments/SectionHeading';
 
 const useStyles = createStyles(
-  (theme, { iconSize = 64 }: { iconSize?: number }) => ({
-    wrapper: {
-      position: 'relative',
+    (theme, {iconSize = 64}: { iconSize?: number }) => ({
+        wrapper: {
+            position: 'relative',
 
-      '&::before': {
-        content: '""',
-        position: 'absolute',
-        top: 30,
-        left: iconSize / 2,
-        bottom: 30,
-        backgroundColor: theme.other.primary,
-        width: 2,
-        zIndex: -10,
-      },
-    },
-    icon: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: iconSize,
-      height: iconSize,
-      aspectRatio: '1 / 1',
-      backgroundColor: theme.other.bg,
-      color: theme.other.primary,
-      borderRadius: '50%',
-    },
+            '&::before': {
+                content: '""',
+                position: 'absolute',
+                top: 30,
+                left: iconSize / 2,
+                bottom: 30,
+                backgroundColor: theme.other.primary,
+                width: 2,
+                zIndex: -10,
+            },
+        },
+        icon: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: iconSize,
+            height: iconSize,
+            aspectRatio: '1 / 1',
+            backgroundColor: theme.other.bg,
+            color: theme.other.primary,
+            borderRadius: '50%',
+        },
 
-    avatarImage: {
-      transform: 'scaleX(-100%)',
-      objectFit: 'contain',
-    },
-  })
+        avatarImage: {
+            transform: 'scaleX(-100%)',
+            objectFit: 'contain',
+        },
+    })
 );
 
 const itemsData = [
-  {
-    title: 'Wstępne warunki i zakres pracy',
-    text: 'Na początku ustalamy satysfakcjonujące nas warunki i podpisujemy dokumenty',
-    icon: IconHeartHandshake,
-  },
-  {
-    title: 'Ustalenia dotyczące wyglądu i funkcjonalności',
-    text: 'Ustalamy przeznaczenie projektu, jego wygląd, możesz przesłać inne projekty, które Ci się spodobały, im więcej informacji tym lepiej!',
-    icon: IconEaseInOutControlPoints,
-  },
-  {
-    title: '*Szukanie inspiracji*',
-    text: 'To czas na zebranie kluczowych danych i pomysłów na projekt.',
-    icon: IconBrain,
-  },
-  {
-    title: 'Akceptacja mockupu',
-    text: 'Przesyłam Ci mój wstępny szkielet tego, jak będzie wyglądać docelowy produkt. Spodoba Ci się? Biorę się do dalszej pracy.',
-    icon: IconMoodCheck,
-  },
-  {
-    title: 'Projekt w budowie',
-    text: 'Prace nad projektem ruszyły i nie zamierzają się zatrzymywać!',
-    icon: IconProgress,
-  },
+    {
+        title: 'Wstępne warunki i zakres pracy',
+        text: 'Na początku ustalamy satysfakcjonujące nas warunki i podpisujemy dokumenty',
+        icon: IconHeartHandshake,
+    },
+    {
+        title: 'Ustalenia dotyczące wyglądu i funkcjonalności',
+        text: 'Ustalamy przeznaczenie projektu, jego wygląd, możesz przesłać inne projekty, które Ci się spodobały, im więcej informacji tym lepiej!',
+        icon: IconEaseInOutControlPoints,
+    },
+    {
+        title: '*Szukanie inspiracji*',
+        text: 'To czas na zebranie kluczowych danych i pomysłów na projekt.',
+        icon: IconBrain,
+    },
+    {
+        title: 'Akceptacja mockupu',
+        text: 'Przesyłam Ci mój wstępny szkielet tego, jak będzie wyglądać docelowy produkt. Spodoba Ci się? Biorę się do dalszej pracy.',
+        icon: IconMoodCheck,
+    },
+    {
+        title: 'Projekt w budowie',
+        text: 'Prace nad projektem ruszyły i nie zamierzają się zatrzymywać!',
+        icon: IconProgress,
+    },
 ];
 
 export function ProjectRoadSection() {
-  const { classes } = useStyles({ iconSize: 64 });
+    const {classes} = useStyles({iconSize: 64});
 
-  return (
-    <Stack mih="60vh" spacing={64} justify="center">
-      <SectionHeading
-        title="Jak wygląda droga projektu?"
-        subtext="Droga od pomysłu do produktu"
-      />
-
-      <Flex
-        direction={{ base: 'column', md: 'row' }}
-        align="center"
-        justify="space-between"
-      >
-        <Stack className={classes.wrapper} spacing={64}>
-          {itemsData.map(({ title, text, icon }) => (
-            <ProjectRoadItem
-              key={title}
-              title={title}
-              text={text}
-              icon={icon}
+    return (
+        <Stack mih="60vh" spacing={64} justify="center">
+            <SectionHeading
+                title="Jak wygląda droga projektu?"
+                subtext="Droga od pomysłu do produktu"
             />
-          ))}
+
+            <Flex
+                direction={{base: 'column', md: 'row'}}
+                align="center"
+                justify="space-between"
+            >
+                <Stack className={classes.wrapper} spacing={64}>
+                    {itemsData.map(({title, text, icon}) => (
+                        <ProjectRoadItem
+                            key={title}
+                            title={title}
+                            text={text}
+                            icon={icon}
+                        />
+                    ))}
+                </Stack>
+                <Image
+                    width={350}
+                    height={350}
+                    src="/avatars/me_blink.png"
+                    alt="Moja twarz z urodzinową trąbką i konfetti."
+                    className={classes.avatarImage}
+                />
+            </Flex>
         </Stack>
-        <Image
-          width={350}
-          height={350}
-          src="/avatars/me_confetti.webp"
-          alt="Moja twarz z urodzinową trąbką i konfetti."
-          className={classes.avatarImage}
-        />
-      </Flex>
-    </Stack>
-  );
+    );
 }
 
 type ProjectRoadItemProps = {
-  title: string;
-  text: string;
-  icon: Icon;
+    title: string;
+    text: string;
+    icon: Icon;
 };
-function ProjectRoadItem({ title, text, icon }: ProjectRoadItemProps) {
-  const { classes } = useStyles({});
-  const IconElement = icon;
 
-  return (
-    <Group noWrap>
-      <Box className={classes.icon}>
-        <IconElement size={40} />
-      </Box>
-      <Stack spacing={8}>
-        <Title order={3}>{title}</Title>
-        <Text textColor="textSecondary" maw={450}>
-          {text}
-        </Text>
-      </Stack>
-    </Group>
-  );
+function ProjectRoadItem({title, text, icon}: ProjectRoadItemProps) {
+    const {classes} = useStyles({});
+    const IconElement = icon;
+
+    return (
+        <Group noWrap>
+            <Box className={classes.icon}>
+                <IconElement size={40}/>
+            </Box>
+            <Stack spacing={8}>
+                <Title order={3}>{title}</Title>
+                <Text textColor="textSecondary" maw={450}>
+                    {text}
+                </Text>
+            </Stack>
+        </Group>
+    );
 }
