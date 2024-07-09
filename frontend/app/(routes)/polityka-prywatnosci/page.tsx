aff27965e0c6d4e1cc810d88c52b7580b66db6a6
Bartosz <barth.webdesign@gmail.com>
2024-07-09 22:10:18 +0200

    [Feat]: add content to privacy policy page

--- frontend/app/(routes)/polityka-prywatnosci/page.tsx
+++ frontend/app/(routes)/polityka-prywatnosci/page.tsx
@@ -2,7 +2,9 @@ import {Metadata} from 'next';
 
 import {getPrivacyPolicyPage} from "@/requests/getPrivacyPolicyPage";
 
-import {Container} from '@/components/common/mantine';
+import {Container, Stack} from '@/components/common/mantine';
+import {Title} from "@/components/common/mantine/Title";
+import {MarkdownContent} from "@/components/common/special/MarkdownContent";
 
 export const metadata: Metadata = {
     title: 'Polityka prywatności | Bartosz Stefaniak',
@@ -10,17 +12,23 @@ export const metadata: Metadata = {
 };
 
 export default async function PrivacyPolicyPage() {
-    // const getPrivacyPolicyPagePromise = getPrivacyPolicyPage().catch(() => ({data: undefined}));
-    // const {data} = await getPrivacyPolicyPagePromise
+    
     const {data} = await getPrivacyPolicyPage().catch((err) => {
         console.error('Error: ', err)
         return {data: undefined}
     })
 
-    console.log("Xdd", data)
     return (
         <Container size="sm" mih="100vh" h="100%" px={24} py={64}>
-            xdd
+            <Stack spacing={16} mb={48}>
+                <Title order={1} size={64} lh={1} textColor="textPrimary">
+                    Polityka prywatności
+                </Title>
+                <Title order={2} textColor="textSecondary">
+                    Znajdziesz tutaj obowiązującą politykę prywatności.
+                </Title>
+                <MarkdownContent content={data?.content}/>
+            </Stack>
         </Container>
     );
 }
