735d58afaab765699283e7eb2b9634c636c42957
Bartosz <barth.webdesign@gmail.com>
2024-07-10 00:29:07 +0200

    [Fix]: landing serpent images optimize

--- frontend/app/components/views/landing/landing/LandingSection.tsx
+++ frontend/app/components/views/landing/landing/LandingSection.tsx
@@ -1,112 +1,114 @@
 'use client';
 
 import React from 'react';
-import { createStyles } from '@mantine/core';
+import {createStyles} from '@mantine/core';
 
 
-import { Box, Container, Stack } from '@/components/common/mantine';
-import { Image } from '@/components/common/mantine/Image';
+import {Box, Container, Stack} from '@/components/common/mantine';
+import {Image} from '@/components/common/mantine/Image';
 
-import { LandingButtons } from './components/LandingButtons';
-import { LandingHeader } from './components/LandingHeader';
+import {LandingButtons} from './components/LandingButtons';
+import {LandingHeader} from './components/LandingHeader';
 
 const useStyles = createStyles((theme) => ({
-  container: {
-    [theme.fn.smallerThan('md')]: {
-      overflowX: 'hidden',
+    container: {
+        [theme.fn.smallerThan('md')]: {
+            overflowX: 'hidden',
+        },
     },
-  },
-  wrapper: {
-    position: 'relative',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    height: '100vh',
-    width: '100%',
-  },
-
-  contentContainer: {
-    position: 'relative',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-    width: '100%',
-    color: theme.other.textPrimary,
-  },
-
-  backgroundImage: {
-    position: 'absolute',
-    top: '20%',
-    width: '100vw',
-    height: 'auto',
-    transform: 'translateX(-20%) translateY(-20%) scale(1.5)',
-    objectFit: 'contain',
-    zIndex: -10,
-
-    [theme.fn.largerThan('md')]: {
-      transform: 'translateX(-20%) translateY(-40%)',
-    },
-    [theme.fn.largerThan('xl')]: {
-      transform: 'translateX(-50%) translateY(-30%)',
-      maxWidth: '50vw',
-    },
-  },
-
-  smallImage: {
-    position: 'absolute',
-    right: 0,
-    bottom: 0,
-    width: '100%',
-    height: 'auto',
-    objectFit: 'contain',
-    transform: 'translateX(40%) scaleX(-1)',
-    zIndex: -10,
-
-    [theme.fn.largerThan('md')]: {
-      width: '50%',
-      bottom: '10%',
-      transform: 'translateX(20%) scaleX(-1)',
+    wrapper: {
+        position: 'relative',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        height: '100vh',
+        width: '100%',
     },
 
-    [theme.fn.largerThan('xl')]: {
-      width: '70%',
-      bottom: '10%',
-      transform: 'translateX(50%) scaleX(-1)',
+    contentContainer: {
+        position: 'relative',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
+        color: theme.other.textPrimary,
     },
-  },
+
+    backgroundImage: {
+        position: 'absolute',
+        top: '20%',
+        width: '100vw',
+        height: 'auto',
+        transform: 'translateX(-20%) translateY(-20%) scale(1.5)',
+        objectFit: 'contain',
+        zIndex: -10,
+
+        [theme.fn.largerThan('md')]: {
+            transform: 'translateX(-20%) translateY(-40%)',
+        },
+        [theme.fn.largerThan('xl')]: {
+            transform: 'translateX(-50%) translateY(-30%)',
+            maxWidth: '50vw',
+        },
+    },
+
+    smallImage: {
+        position: 'absolute',
+        right: 0,
+        bottom: 0,
+        width: '100%',
+        height: 'auto',
+        objectFit: 'contain',
+        transform: 'translateX(40%) scaleX(-1)',
+        zIndex: -10,
+
+        [theme.fn.largerThan('md')]: {
+            width: '50%',
+            bottom: '10%',
+            transform: 'translateX(20%) scaleX(-1)',
+        },
+
+        [theme.fn.largerThan('xl')]: {
+            width: '70%',
+            bottom: '10%',
+            transform: 'translateX(50%) scaleX(-1)',
+        },
+    },
 }));
 
 export function LandingSection() {
-  const { classes } = useStyles();
+    const {classes} = useStyles();
 
-  return (
-    <Container size="lg" className={classes.container} h="100%">
-      <Box component="section" className={classes.wrapper}>
-        <Image
-          src="/abstract/serpent_3.webp"
-          alt="Kształt przypominający węża, z kolorowym gradientem"
-          height="0"
-          width="0"
-          sizes="100vw"
-          priority
-          className={classes.backgroundImage}
-        />
-        <Image
-          src="/abstract/serpent_2.webp"
-          alt="Półokragły, kolorowy kształt"
-          height="0"
-          width="0"
-          sizes="100vw"
-          priority
-          className={classes.smallImage}
-        />
+    return (
+        <Container size="lg" className={classes.container} h="100%">
+            <Box component="section" className={classes.wrapper}>
+                <Image
+                    src="/abstract/serpent_3.webp"
+                    alt="Kształt przypominający węża, z kolorowym gradientem"
+                    height={600}
+                    width={420}
+                    sizes="50vw"
+                    loading='eager'
+                    priority
+                    className={classes.backgroundImage}
+                />
+                <Image
+                    src="/abstract/serpent_2.webp"
+                    alt="Półokragły, kolorowy kształt"
+                    height={550}
+                    width={550}
+                    sizes="50vw"
+                    loading='eager'
+                    priority
+                    className={classes.smallImage}
+                />
 
-        <Stack w="100%">
-          <LandingHeader />
-          <LandingButtons />
-        </Stack>
-      </Box>
-    </Container>
-  );
+                <Stack w="100%">
+                    <LandingHeader/>
+                    <LandingButtons/>
+                </Stack>
+            </Box>
+        </Container>
+    );
 }
